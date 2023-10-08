a9beeb320230ba0b31a9946b7f6ea29f8e5eaa24
MEskerod <112616513+MEskerod@users.noreply.github.com>
2023-10-08 19:50:58 +0200

    Added function

--- fold_methods/main.py
+++ fold_methods/main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 ### FILES USED BY MULTIPLE FILES IN FOLDER ###  
 
 def db_to_file(sequence, db, filename, name): 
@@ -6,3 +8,16 @@ def db_to_file(sequence, db, filename, name):
         f.write(f"#Length: {len(sequence)}\n")
         f.write(sequence + "\n")
         f.write(db + "\n")
+
+
+def read_parameter(file_stacking): 
+    """
+
+    """
+    try:
+        stacking = pd.read_csv(file_stacking, index_col=0)
+    except FileNotFoundError:
+        raise FileNotFoundError("Parameter file not found")
+    except pd.errors.EmptyDataError:
+        raise ValueError("Parameter file is empty or in an unexpected file format")
+    return stacking
